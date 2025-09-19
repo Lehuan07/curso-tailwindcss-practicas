e9886e70d88687dc4feaa22e4bb705a65b4814b5
Nahuel C. <nahuelncejas@gmail.com>
2025-09-19 10:47:23 -0300

    Eso

--- TaiwilndConReact/cursoPadre/curso-react-taildwin-css-hijo/src/pages/Home.jsx
+++ TaiwilndConReact/cursoPadre/curso-react-taildwin-css-hijo/src/pages/Home.jsx
@@ -5,7 +5,7 @@ export const Home = () => {
     return (
         <section className="bg-amber-50 h-screen dark:bg-dark">
             Home
-            <button className="p-2 rounded-lg bg-white dark:bg-dark" onClick={toggleTheme}>
+            <button className="p-2 rounded-lg bg-amber-200 dark:bg-[#8b8b8b] font-semibold" onClick={toggleTheme}>
                 {theme==="light"?"🌞 Modo claro":"🌚 Modo oscuro"}
             </button>
             </section>
--- TaiwilndConReact/cursoPadre/curso-react-taildwin-css-hijo/src/store/useThemeStore.jsx
+++ TaiwilndConReact/cursoPadre/curso-react-taildwin-css-hijo/src/store/useThemeStore.jsx
@@ -17,6 +17,4 @@ export const useThemeStore = create(
             name:"theme-Storage"
         }
     )
-        
-    
 )
